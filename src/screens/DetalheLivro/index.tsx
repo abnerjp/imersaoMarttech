98c669f43d35507516ee449eec09f902e3c749bf
abnerjp-w10 <abner.pelisser@gmail.com>
2020-12-30 02:38:55 -0300

    implementando botão para adicionar/remover favoritos, na tela de detalhes do livro.

--- src/screens/DetalheLivro/index.tsx
+++ src/screens/DetalheLivro/index.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { TouchableOpacity, View, StatusBar, ActivityIndicator } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
+import { ListaLivrosDTO } from '../ListaLivros/index';
 import { 
   Container,
   ContainerRolagem,
@@ -14,9 +15,9 @@ import {
   TituloDescricao,
   TextoDescricao,
   BotaoFavoritos,
-  TextoFavoritos
+  TextoFavoritos,
 } from './styles';
-import { buscaDetalheLivro } from '../../services/livro';
+import { buscaDetalheLivro, listaFavoritos, favoritarLivro, desfavoritarLivro } from '../../services/livro';
 
 interface DetalheLivroDTO {
   id: number;
@@ -27,55 +28,91 @@ interface DetalheLivroDTO {
 }
 
 const DetalheLivro = () => {
-    const navigation = useNavigation();
-    const route = useRoute();
-    const [
-      detalheLivro, 
-      setDetalheLivro
-    ] = useState<DetalheLivroDTO | null>(null);
+  const [detalheLivro, setDetalheLivro] = useState<DetalheLivroDTO | null>(null);
+  const [livrosFavoritos, setLivrosFavoritos] = useState<ListaLivrosDTO[]>([]);
+  const navigation = useNavigation();
+  const route = useRoute();
+  const [isFavorito, setIsFavorito] = useState(false);
 
-    useEffect(() => {
-      const carregaDetalheLivro = async () => {
-        const livroId = route.params.livroId;
-        const resposta = await buscaDetalheLivro(livroId);
-        setDetalheLivro((await resposta.json()).data);
-      };
+  useEffect(() => {
+    const carregaDetalheLivro = async () => {
+      const livroId = route.params.livroId;
+
+      /* 
+        carrega a lista de livros favoritos, para verificar se livro detalhado é um favorito 
+        nao é a melhor maneira de verificar se 1 livro é favorito, mas foi a forma encontrada
+      */
+      const respostaFavoritos = await listaFavoritos();
       
-      carregaDetalheLivro();
-    }, []);
+      //setLivrosFavoritos((await respostaFavoritos.json()).data);
+      console.log((await respostaFavoritos.json()));
+      setIsFavorito(!! livrosFavoritos.find(livro => livro.id === livroId));
 
-    if (detalheLivro === null) {
-      return <View style={{ flex: 1, justifyContent: 'center', alignContent: 'center'}} >
-        <ActivityIndicator size={42} color="#023E8A" />
-      </View>;
-    }
+      const resposta = await buscaDetalheLivro(livroId);
+      setDetalheLivro((await resposta.json()).data);
+    };
     
-    return (
-    <>
-        <StatusBar barStyle="dark-content" backgroundColor="#E7F5F8" />
-        <Container>
-            <ContainerRolagem>
-              <ContainerInfoPrincipalLivro>
-                <View>
-                  <TouchableOpacity onPress={() => { navigation.goBack(); }} >
-                    <Icon name="arrow-left" size={24} color="#000" />
-                  </TouchableOpacity>
-                </View>
-                <ImagemLivro source={{ uri: detalheLivro?.imagem }} resizeMode="contain" />
-                <NomeLivro> {detalheLivro?.nome} </NomeLivro>
-                <NomeAutor> {detalheLivro?.autor} </NomeAutor>
-              </ContainerInfoPrincipalLivro>
-              <ContainerDescricao>
-                <TituloDescricao> Descrição </TituloDescricao>
-                <TextoDescricao> {detalheLivro?.descricao} </TextoDescricao>
-              </ContainerDescricao>
-            </ContainerRolagem>
-          <BotaoFavoritos>
-            <TextoFavoritos> Adicionar aos favoritos </TextoFavoritos>
-          </BotaoFavoritos>
-        </Container>
-    </>
-    );
+    carregaDetalheLivro();
+  }, []);
+
+  async function clickedFavorite() {
+    if (!!detalheLivro) {
+      var resp = null;
+      console.log('livroID:', detalheLivro.id);
+      if (isFavorito)  {
+        resp = await favoritarLivro(detalheLivro.id);
+      } 
+      else {
+        resp = await desfavoritarLivro(detalheLivro.id);
+      }
+      console.log('clicked: --> ',JSON.stringify(resp));
+      if (!!resp) {
+        setIsFavorito(!isFavorito);
+      }
+    }
+  }
+
+  if (detalheLivro === null) {
+    return <View style={{ flex: 1, justifyContent: 'center', alignContent: 'center'}} >
+      <ActivityIndicator size={42} color="#023E8A" />
+    </View>;
+  }
+  
+  return (
+  <>
+      <StatusBar barStyle="dark-content" backgroundColor="#E7F5F8" />
+      <Container>
+          <ContainerRolagem>
+            <ContainerInfoPrincipalLivro>
+              <View>
+                <TouchableOpacity onPress={() => { navigation.goBack(); }} >
+                  <Icon name="arrow-left" size={24} color="#000" />
+                </TouchableOpacity>
+              </View>
+              <ImagemLivro source={{ uri: detalheLivro?.imagem }} resizeMode="contain" />
+              <NomeLivro> {detalheLivro?.nome} </NomeLivro>
+              <NomeAutor> {detalheLivro?.autor} </NomeAutor>
+            </ContainerInfoPrincipalLivro>
+            <ContainerDescricao>
+              <TituloDescricao> Descrição </TituloDescricao>
+              <TextoDescricao> {detalheLivro?.descricao} </TextoDescricao>
+            </ContainerDescricao>
+          </ContainerRolagem>
+
+        <BotaoFavoritos 
+          style={ isFavorito ? { backgroundColor: '#E6E6E6'} : {}}
+          onPress={clickedFavorite}
+        >
+          <TextoFavoritos 
+            style={ isFavorito ? { color: '#000000'} : {}}
+          > 
+            {isFavorito ? 'Remover dos favoritos' : 'Adicionar aos favoritos'}
+          </TextoFavoritos>
+        </BotaoFavoritos>
+
+      </Container>
+  </>
+  );
 }
 
 export default DetalheLivro;
--- src/services/livro.ts
+++ src/services/livro.ts
@@ -20,3 +20,18 @@ export const buscaDetalheLivro = async(livroId:number) => {
   const resposta = await fetch(`https://scot13.tst.marttech.com.br/api/Livro/ListarDetalheLivro/${livroId}`, await header());
   return resposta;
 }
+
+export const listaFavoritos = async() => {
+  const resposta = await fetch(`https://scot13.tst.marttech.com.br/api/Favorito/ListarLivrosFavoritados`, await header());
+  return resposta;
+}
+
+export const favoritarLivro = async(livroId:number) => {
+  const resposta = await fetch(`https://scot13.tst.marttech.com.br/api/Favorito/api/Favorito/FavoritarLivro/${livroId}`, await header());
+  return resposta;
+}
+
+export const desfavoritarLivro = async(livroId:number) => {
+  const resposta = await fetch(`https://scot13.tst.marttech.com.br/api/Favorito/api/Favorito/DesfavoritarLivro/${livroId}`, await header());
+  return resposta;
+}
